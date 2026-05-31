352d8e0103823842a0367280760a784030ab77ce
Ubuntu <ubuntu@ip-172-31-23-133.ap-southeast-2.compute.internal>
2026-05-31 10:23:35 +0000

    3 number multiplication

--- calculator.sh
+++ calculator.sh
@@ -11,3 +11,9 @@ echo "Sub is = $suba"
 
 mul=$((a*b))
 echo "Mul is = $mul"
+
+read -p "Enter first number : " m
+read -p "Enter second number : " n
+read -p "Enter third number : " o
+mul3=$((m*n*o))
+echo "Mul3 is = $mul3"
