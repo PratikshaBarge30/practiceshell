93e90e597951e7d48c23945004580b2619d467b2
Ubuntu <ubuntu@ip-172-31-23-133.ap-southeast-2.compute.internal>
2026-05-31 10:28:35 +0000

    Multiplication of 4 commit

--- calculator.sh
+++ calculator.sh
@@ -17,3 +17,10 @@ read -p "Enter second number : " n
 read -p "Enter third number : " o
 mul3=$((m*n*o))
 echo "Mul3 is = $mul3"
+
+read -p "Enter first number : " a
+read -p "Enter second number : " b
+read -p "Enter third number : " c
+read -p "Enter forth number : " d
+mul4=$((a*b*c*d))
+echo "Multiplication of 4 = $mul4"
